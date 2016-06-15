fff7c0cfe3cdd2391236f8e0a75c84f736a3fa69
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-14 21:45:04 -0700

    Initial Commit

---
+++ CS50TileGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS50TileGame
+//
+//  Created by James Dylan Goldstein on 6/14/16.
+//  Copyright © 2016 James Dylan Goldstein. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
